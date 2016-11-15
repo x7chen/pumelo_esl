624aa218f4d96c4830391c9f15ad4fdee97ff5b4
coder <code@pumelotech.com>
2016-11-15 18:18:01 +0800

    cp

--- app/main/lable_data.c
+++ app/main/lable_data.c
@@ -5,6 +5,7 @@
 #include "ble_esls.h"
 #define ONE_PACKET_DATA_LENGTH 16
 uint16_t sequency = 0;
+bool is_transporting = 0;
 uint8_t __attribute((aligned (4))) aligned_data[20];
 
 uint16_t get_packet_index(uint8_t * packet)
@@ -26,15 +27,28 @@ void esl_packet_handle(ble_esls_t * p_esls, uint8_t * packet,uint16_t length)
     {
         return;
     }
-    esl_flash_store(	sequency*ONE_PACKET_DATA_LENGTH,
-			get_packet_data_aligned(packet),
-			4,
-			NULL);
+    esl_flash_store(sequency*ONE_PACKET_DATA_LENGTH,
+			        get_packet_data_aligned(packet),
+			        4,
+			        NULL);
     sequency++;
 }
+/*****************************************************************************
+ * 0x00:初始化,设定数据长度和CRC16，启动传输过程
+ * 0x01:结束传输，验证数据完整性，并返回成功标志
+*****************************************************************************/
 void esl_cp_handle(ble_esls_t * p_esls, uint8_t * control_point,uint16_t length)
 {
-    
+    switch(control_point[0])
+    {
+        case 0x00:
+            sequency = 0;
+            break;
+        case 0x01:
+            break;
+        default:
+            break;
+    }
 }
 void esl_cp_resp(ble_esls_t * p_esls, uint8_t * resp,uint16_t length)
 {
--- app/main/lable_data.h
+++ app/main/lable_data.h
@@ -7,14 +7,16 @@ typedef struct
     uint16_t length;
     uint16_t crc16;
 }esl_data_t;
+typedef enum
+{
+    TRANSPORT_INIT=0,
+    TRANSPORT_OVER=1,
+    RESPOND=10,
+}cp_command_t;
 
 void esl_packet_handle(ble_esls_t * p_esls, uint8_t * packet,uint16_t length);
 void esl_cp_handle(ble_esls_t * p_esls, uint8_t * control_point,uint16_t length);
 void esl_cp_resp(ble_esls_t * p_esls, uint8_t * resp,uint16_t length);
-
-
-
-
 
 #endif
 
